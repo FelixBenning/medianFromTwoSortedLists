161196f19091918fcccb57be7b16ca858df91566
Felix Benning <felix.benning@gmail.com>
2021-05-10 09:44:44 +0000

    working unbalanced test

--- search_median.py
+++ search_median.py
@@ -1,4 +1,19 @@
+from math import floor
 from statistics import median
 
-def brute_force_median(long_list, short_list):
-    pass
+
+def brute_force_median(max_4, max_2):
+    return median(max_4 + max_2)
+
+
+def median_unbalanced(long_list, short_list):
+    """len(short_list) <= 2!!, neither of the lists should be empty, no nan, inf"""
+    if len(long_list) <= 4:
+        return brute_force_median(long_list, short_list)
+    med = (len(long_list) - 1) / 2
+    lmed = floor(med)
+    need = 3 if lmed == med else 4
+    return brute_force_median(
+        [long_list[lmed - 1 + idx] for idx in range(need)], short_list
+    )
+
--- test_search_median.py
+++ test_search_median.py
@@ -1,7 +1,18 @@
 from statistics import median
-from hypothesis import given, strategies as st
-from search_median import brute_force_median
+from hypothesis import given, strategies as st, example
+from search_median import median_unbalanced
 
-@given(long=st.lists(elements=st.floats(), min_size=1), short=st.lists(st.floats(), max_size=2))
-def test_brute_foce_median(long, short):
-    assert median(long + short) == brute_force_median(long, short)
+
+@given(
+    long=st.lists(
+        elements=st.floats(allow_nan=False, allow_infinity=False), min_size=1
+    ),
+    short=st.lists(
+        st.floats(allow_nan=False, allow_infinity=False), min_size=1, max_size=2
+    ),
+)
+@example(long=[0.0, 0.0, 0.0, -1.0, 0.0], short=[-1.0])
+def test_median_unbalanced(long, short):
+    long.sort()
+    short.sort()
+    assert median(long + short) == median_unbalanced(long, short)
