367ce155d656d715871e50ac5f8fa17d25450e74
Felix Benning <felix.benning@gmail.com>
2021-05-10 14:39:38 +0000

    replace numpy with view class

--- search_median.py
+++ search_median.py
@@ -1,18 +1,46 @@
-import numpy as np
 from math import floor, ceil
 from statistics import median
 
 
+class View:
+    def __init__(self, vec):
+        self.vec = vec
+        self.start = 0
+        self.length = len(vec)
+
+    def reduce_left(self, amount):
+        self.start += amount
+        self.length -= amount
+
+    def reduce_right(self, amount):
+        self.length -= amount
+
+    def reduce_slice(self, left, right):
+        self.start += left
+        assert self.length > right
+        self.length = right - left
+
+    def reduce_to(self, left, size):
+        self.start += left
+        self.length = size
+
+    def __getitem__(self, key):
+        if key < self.length:
+            return self.vec[self.start + key]
+        else:
+            raise IndexError
+
+
 def brute_force_median(list_a, list_b):
-    med = floor((len(list_a) + len(list_b))/2)
-    need_one = bool((len(list_a) + len(list_b)) % 2)
+    med = floor((list_a.length + list_b.length) / 2)
+    need_one = bool((list_a.length + list_b.length) % 2)
     idx_a = 0
     idx_b = 0
     for _ in range(med + need_one):
-        if idx_b >= len(list_b):
+        if idx_b >= list_b.length:
             first_med = list_a[idx_a]
             idx_a += 1
-        elif idx_a >= len(list_a):
+        elif idx_a >= list_a.length:
             first_med = list_b[idx_b]
             idx_b += 1
         elif list_a[idx_a] < list_b[idx_b]:
@@ -25,49 +53,47 @@ def brute_force_median(list_a, list_b):
     if need_one:
         return first_med
 
-    if idx_b >= len(list_b):
-        return (first_med + list_a[idx_a])/2
-    elif idx_a >= len(list_a):
-        return (first_med + list_b[idx_b])/2
+    if idx_b >= list_b.length:
+        return (first_med + list_a[idx_a]) / 2
+    elif idx_a >= list_a.length:
+        return (first_med + list_b[idx_b]) / 2
     elif list_a[idx_a] < list_b[idx_b]:
-        return (first_med + list_a[idx_a])/2
-    return (first_med + list_b[idx_b])/2
+        return (first_med + list_a[idx_a]) / 2
+    return (first_med + list_b[idx_b]) / 2
 
 
 def median_unbalanced(long_list, short_list):
     """len(short_list) <= 2!!, neither of the lists should be empty, no nan, inf
     both of them should be sorted"""
-    if len(long_list) <= 4:
+    if long_list.length <= 4:
         return brute_force_median(long_list, short_list)
-    need = 3 if len(long_list) % 2 else 4
-    left_from_med = floor((len(long_list) - 1) / 2) - 1
-    return brute_force_median(
-        long_list[left_from_med : left_from_med + need], short_list
-    )
+    need = 3 if long_list.length % 2 else 4
+    left_from_med = floor((long_list.length - 1) / 2) - 1
+    long_list.reduce_to(left_from_med, need)
+    return brute_force_median(long_list, short_list)
 
 
 def median_two_sorted(list_a, list_b):
     if len(list_a) < len(list_b):
-        return median_reduction(list_b, list_a)
+        return median_reduction(View(list_b), View(list_a))
     else:
-        return median_reduction(list_a, list_b)
+        return median_reduction(View(list_a), View(list_b))
 
 
 def median_reduction(long_list, short_list):
-    while len(short_list) > 2:
-        short_med = (len(short_list) - 1) / 2
+    while short_list.length > 2:
+        short_med = (short_list.length - 1) / 2
         short_lmed = floor(short_med)
-        reduction = len(short_list) - short_lmed - 1
-        long_med = (len(long_list) - 1) / 2
+        reduction = short_list.length - short_lmed - 1
+        long_med = (long_list.length - 1) / 2
         if long_list[ceil(long_med)] < short_list[short_lmed]:
-            long_list = long_list[reduction:]
-            short_list = short_list[:-reduction]
+            long_list.reduce_left(reduction)
+            short_list.reduce_right(reduction)
         elif short_list[ceil(short_med)] < long_list[floor(long_med)]:
-            long_list = long_list[:-reduction]
-            short_list = short_list[reduction:]
+            long_list.reduce_right(reduction)
+            short_list.reduce_left(reduction)
         else:
-            return brute_force_median(
-                long_list[floor(long_med) : ceil(long_med) + 1],
-                short_list[short_lmed : ceil(short_med) + 1],
-            )
+            long_list.reduce_slice(floor(long_med), ceil(long_med) + 1)
+            short_list.reduce_slice(short_lmed, ceil(short_med) + 1)
+            return brute_force_median(long_list, short_list)
     return median_unbalanced(long_list, short_list)
--- test_search_median.py
+++ test_search_median.py
@@ -1,7 +1,6 @@
 from statistics import median
-import numpy as np
 from hypothesis import given, strategies as st, example
-from search_median import median_unbalanced, median_two_sorted
+from search_median import median_unbalanced, median_two_sorted, View
 
 
 @given(
@@ -12,21 +11,19 @@ from search_median import median_unbalanced, median_two_sorted
         st.floats(allow_nan=False, allow_infinity=False), min_size=1, max_size=2
     ),
 )
-@example(long=[0.0], short=[0.0])
+@example(long=[0.0, 0.0, 0.0, 0.0, 0.0], short=[1.0])
 def test_median_unbalanced(long, short):
     long.sort()
     short.sort()
-    assert median(long + short) == median_unbalanced(np.array(long), np.array(short))
+    assert median(long + short) == median_unbalanced(View(long), View(short))
 
 
 @given(
     list_a=st.lists(st.floats(allow_nan=False, allow_infinity=False), min_size=1),
     list_b=st.lists(st.floats(allow_nan=False, allow_infinity=False), min_size=1),
 )
-@example(list_a=[0.0], list_b=[0.0, -1.0])
+@example(list_a=[0.0, 0.0, 0.0], list_b=[0.0, -1.0, -1.0])
 def test_median_two_sorted(list_a, list_b):
     list_a.sort()
     list_b.sort()
-    assert median(list_a + list_b) == median_two_sorted(
-        np.array(list_a), np.array(list_b)
-    )
+    assert median(list_a + list_b) == median_two_sorted(list_a, list_b)
